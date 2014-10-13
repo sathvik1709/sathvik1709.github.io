eb925e7a5843dcebda248d97f263516746bdd30d
sathvik1709 <sathvikshivaprakash519@gmail.com>
2014-10-13 03:39:44 -0500

    added jquery jqueryui library

---
+++ change_color.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $('.menu_item_div').mouseenter(function(){
+        $(this).fadeTo('fast',1);
+    });
+    
+    $('.menu_item_div').mouseleave(function(){
+        $(this).fadeTo('fast',0.75);
+    });
+    
+    
+    
+});
+
