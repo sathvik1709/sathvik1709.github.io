a573450c498be7f6aba06a08dc60f35ea6317a7d
sathvik1709 <sathvikshivaprakash519@gmail.com>
2014-10-13 03:43:48 -0500

    minor changes

--- change_color.js
+++ change_color.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     
     $('.menu_item_div').mouseenter(function(){
-        $(this).fadeTo('fast',1);
+        $(this).fadeTo('fast',0.7);
     });
     
     $('.menu_item_div').mouseleave(function(){
-        $(this).fadeTo('fast',0.75);
+        $(this).fadeTo('fast',1);
     });
     
     
